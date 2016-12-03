9cf89684c6111be01eeeca13902e99769e679e26
Jorge Sepulveda <gsepulveda9697@gmail.com>
2016-12-03 06:57:11 +0000

    changed money display at top

--- Main.cpp
+++ Main.cpp
@@ -514,7 +514,7 @@ try{
         {
             // Initialize bank money. Bank always stores money in USD.
             bank.setDefaultSymbol( static_cast<Symbol>(i) );
-            Money m( Symbol::USD, 100000.00 );
+            Money m( Symbol::USD, 100000.00*xRateFromTo(Symbol::USD, static_cast<Symbol>(i) ) );
             bank.setMoney( m );
         }
 
